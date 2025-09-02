7f5f1ec1e9766764adecfa7fde03cd8b0b616932
Ankit Kumar <ankitsharmabela@gmail.com>
2025-09-02 17:13:08 +0530

    some changes made

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,30 @@
 const toggleButton=document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-const navLinkItems = document.querySelectorAll(".nav-link");
+const navLinks=document.querySelector(".nav-links");
+const navLinkItems=document.querySelectorAll(".nav-link");
+const menuIcon=toggleButton.querySelector("i");
+
 toggleButton.addEventListener("click", ()=>{
-  if (navLinks.style.display==="flex")
+  const visible=navLinks.style.display==="flex";
+  if(visible)
      {
-    navLinks.style.display = "none";//hide
-  } else {
-    navLinks.style.display = "flex";//show
+    navLinks.style.display = "none";
+     menuIcon.classList.remove("fa-xmark"); 
+    menuIcon.classList.add("fa-bars");
+  } 
+  else 
+    {
+    navLinks.style.display = "flex";
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
   }})
 
 navLinkItems.forEach(link => {
-  link.addEventListener("click",() => {
+  link.addEventListener("click",() =>
+     {
     navLinks.style.display = "none";
+     menuIcon.classList.remove("fa-xmark"); 
+    menuIcon.classList.add("fa-bars");
   });
 })
+
+
